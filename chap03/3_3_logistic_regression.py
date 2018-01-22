c657746377107a166c909616e1e1b9f2b3c87559
Hao Wang <wanghao10@xiaomi.com>
2018-01-22 21:26:32 +0800

    Logistic regression.

--- chap03/3_3_logistic_regression.py
+++ chap03/3_3_logistic_regression.py
@@ -7,36 +7,86 @@ Task: 识别西瓜好坏
 Performance: 判断结果的准确率
 Experience: 西瓜数据集3.0_alpha
 """
-
+import math
 import numpy as np
 import pandas as pd
-import scipy.optimization
-
 
 def logit(x, thresh = 0.5):
-    return 1 if (1/(1+math.exp(-x))>thresh) else 0
-
-def loss_rms(X, y, w, b):
     """
     Input:
-        X: np.array([M, N])
-        y: np.array(M)
-        w: np.array(N)
-        b: float
+        x: float
+        thresh: float
+    Return:
+        logit: 0 or 1
+    """
+    #return 1 if (1/(1+math.exp(-x))>thresh) else 0
+    return (1/(1+math.exp(-x)))>thresh
+
+
+def loss_cross_entropy(X, y, w):
+    """
+    Cross entropy as loss. 
+
+    Input:
+        X: input data, np.array([M, N])
+        y: labels, np.array(M)
+        w: parameters, np.array(N)
 
     Return: 
         loss: float
     """
-    loss = sum([(logit(_)-answer)*(logit(_)-answer) 
-        for (_, answer) in zip(np.dot(X, w) + b, y)])
-
+    loss = 0
+    N = X.shape[1]
+    for idx in range(X.shape[1]):
+        loss += 1./N * np.log(1+np.exp(-y[idx]*np.dot(w, X[idx, :])))
     return loss
    
-def find_best_model(df, y_col = '好瓜'):
-    X = df.loc[:, df.columns != y_col].values
-    y = df[y_col].values
 
-    n_params = len(df.columns) - 1
-    p = {'w':np.ones(n_params), 'b':0., 'X':X, 'y'=y}
-    best = find_minimum(loss_rms, **p)
-    return best
+def loss_grad_wrt_w(X, y, w):
+    """
+    Input:
+        X: np.array 
+        y: np.array of labels, -1/1
+
+    Return:
+        loss_grad: float
+    """
+    N = len(X)
+    loss_grad = 0
+    for i in range(N):
+        loss_grad += -(1./(1+np.exp(-y[i] * (np.dot(w, X[i, :])))) * 
+                np.exp(-y[i] * (np.dot(w, X[i, :]))) *
+                (y[i]*X[i, :]))
+
+    loss = loss_cross_entropy(X, y, w)
+    #print("loss: " + str(loss))
+    return loss_grad/N
+
+
+def find_best_model(X, y, w, learn_rate, time_steps):
+    for _ in range(time_steps): 
+        w = w - loss_grad_wrt_w(X, y, w)*learn_rate
+        #print(w)
+
+    return w
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/xigua_3.0_alpha.csv')
+    y_col = '好瓜'
+
+    # Add the bias column.
+    X = df.loc[:, df.columns != y_col].values
+    const_col = X.sum(1)[...,None]
+    X = np.append(X, const_col, 1)
+
+    # For using cross-entropy loss function, make the 1/0 binary value to be 1/-1
+    y = df[y_col].values
+    y[y==0] = -1
+
+    # Find the best parameter using gradient descent
+    w = np.ones(X.shape[1])
+    w = find_best_model(X, y, w, 0.5, 500)
+
+    df['pred'] = [logit(f) for f in np.dot(X, w)]
+    print(df)
--- chap03/interesting_topics.md
+++ chap03/interesting_topics.md
@@ -1,1 +1,2 @@
 ** Convex optimization: <http://www.scipy-lectures.org/advanced/mathematical_optimization/#convex-versus-non-convex-optimization>
+** LDA *vs* SVM
